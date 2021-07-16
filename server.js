7ef04adb5a9f46f79c6cd0986995d9eaba572a4d
ohse-emily <82620945+ohse-emily@users.noreply.github.com>
2021-07-16 12:30:53 +0900

    'update'

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send('hi')
+})
+
+app.listen(3000,()=>{
+    console.log(`server start port: 3000`)
+})
